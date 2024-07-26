8b85b3ea5d6951cb6e4e13560c0693a0bd92c3ac
Harald Braeuning <h.braeuning@gsi.de>
2024-07-26 21:00:48 +0200

    license header

--- src/chartwx/chartobject.cpp
+++ src/chartwx/chartobject.cpp
@@ -1,3 +1,25 @@
+/********************************************************************************
+ *                                                                              *
+ * chartwx - chart object base class                                            *
+ *                                                                              *
+ * modified: 2024-07-26                                                         *
+ *                                                                              *
+ ********************************************************************************
+ * Copyright (C) Harald Braeuning                                               *
+ ********************************************************************************
+ * This file is part of 'chartwx'.                                              *
+ * 'chartwx' is free software: you can redistribute it and/or modify it         *
+ * under the terms of the GNU General Public License as published by the Free   *
+ * Software Foundation, either version 3 of the License, or (at your option)    *
+ * any later version.                                                           *
+ * 'chartwx' is distributed in the hope that it will be useful, but             *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for  *
+ * more details.                                                                *
+ * You should have received a copy of the GNU General Public License along with *
+ * imca. If not, see <https://www.gnu.org/licenses/>.                           *
+ ********************************************************************************/
+
 #include "chartobject.h"
 
 namespace chartwx {
